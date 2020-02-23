a19a1df06657a2bf225d4cf4a72c7d4f37ef6ed2
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2020-02-22 19:52:11 -0500

    feat: Initial commit.

---
+++ babel.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        useBuiltIns: "entry",
+        corejs: 3,
+      },
+    ],
+    "@babel/preset-typescript",
+    "@babel/preset-react",
+    [
+      "@emotion/babel-preset-css-prop",
+      {
+        autoLabel: process.env.NODE_ENV !== "production",
+        labelFormat: "[local]",
+      },
+    ],
+  ],
+  plugins: [
+    "@babel/plugin-transform-runtime",
+    "@babel/plugin-proposal-class-properties",
+  ],
+}
