c4afdf9ac0f5748ea56a1c15d3cee60eafc139e8
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2020-02-24 14:30:30 -0500

    test: Adds test.

--- tests/NotificationsProvider.test.tsx
+++ tests/NotificationsProvider.test.tsx
@@ -1,8 +1,9 @@
 /** @jsx jsx */
-import { ReactElement } from "react"
+import { ReactElement, ReactNode } from "react"
 import { jsx } from "@emotion/core"
 import { mount } from "enzyme"
 import { matchers } from "jest-emotion"
+import { uniqueId } from "underscore"
 //import waitForExpect from "wait-for-expect"
 
 expect.extend(matchers)
@@ -18,13 +19,11 @@ const renderNotification = (text: string): ReactElement => (
 describe("NotificationsProvider", () => {
   it("should render a container at the bottom (bottom: 0) the dom", () => {
     const wrapper = mount(
-      <div id={"app"}>
-        <NotificationsProvider
-          displayInterval={3000}
-          renderNotification={renderNotification}>
-          <div id={"page"}>Page</div>
-        </NotificationsProvider>
-      </div>
+      <NotificationsProvider
+        displayInterval={3000}
+        renderNotification={renderNotification}>
+        <div id={"page"}>Page</div>
+      </NotificationsProvider>
     )
 
     expect(wrapper.find("div.Notifications")).toHaveLength(1)
@@ -35,14 +34,12 @@ describe("NotificationsProvider", () => {
     const handleScroll = jest.fn()
 
     const wrapper = mount(
-      <div id={"app"}>
-        <NotificationsProvider
-          displayInterval={3000}
-          renderNotification={renderNotification}
-          onScroll={handleScroll}>
-          <div id={"page"}>Page</div>
-        </NotificationsProvider>
-      </div>
+      <NotificationsProvider
+        displayInterval={3000}
+        renderNotification={renderNotification}
+        onScroll={handleScroll}>
+        <div id={"page"}>Page</div>
+      </NotificationsProvider>
     )
 
     document.documentElement.scrollTop = 100
@@ -67,7 +64,7 @@ describe("NotificationsProvider", () => {
     expect(wrapper.find("div.Notifications")).toHaveStyleRule("bottom", "0")
   })
 
-  it("should render notifications using <NotificationMessage />, then remove it", () => {
+  it("should render notification using <NotificationMessage />, then remove it", () => {
     const PageWithNotifications = withNotifications(
       ({ NotificationMessage }: NotificationsContextProps): ReactElement => (
         <div id={"page"}>
@@ -80,19 +77,79 @@ describe("NotificationsProvider", () => {
     )
 
     const wrapper = mount(
-      <div id={"app"}>
-        <NotificationsProvider
-          displayInterval={3000}
-          renderNotification={renderNotification}>
-          <PageWithNotifications />
-        </NotificationsProvider>
-      </div>
+      <NotificationsProvider
+        displayInterval={3000}
+        renderNotification={renderNotification}>
+        <PageWithNotifications />
+      </NotificationsProvider>
     )
 
-    expect(wrapper.find("div.notification")).toHaveLength(1)
+    expect(wrapper.find("div.Notification")).toHaveLength(1)
 
     setTimeout(() => {
-      expect(wrapper.find(".div.notification")).toHaveLength(0)
+      expect(wrapper.find(".div.Notification")).toHaveLength(0)
     }, 3000)
+  })
+
+  it("should render repeated notifications using createNotificationMessage, then remove one at a time", () => {
+    interface ButtonProps {
+      onClick: () => void;
+    }
+
+    const Button = ({ onClick }: ButtonProps): ReactElement => (
+      <button type={"button"} onClick={onClick}>
+        Notification Button
+      </button>
+    )
+
+    const Page = withNotifications(
+      ({
+        createNotificationMessage,
+      }: NotificationsContextProps): ReactElement => (
+        <div id={"page"}>
+          <h1>Page</h1>
+          <Button
+            onClick={(): void => {
+              createNotificationMessage({
+                notificationName: `notification-${uniqueId()}`,
+                isRepeated: true,
+                onRender: () => <div>This is a repeated notification</div>,
+              })
+            }}
+          />
+        </div>
+      )
+    )
+
+    const displayInterval = 3000
+    const wrapper = mount(
+      <NotificationsProvider
+        displayInterval={displayInterval}
+        renderNotification={renderNotification}>
+        <Page />
+      </NotificationsProvider>
+    )
+
+    expect(wrapper.find("div.Notification")).toHaveLength(0)
+
+    const btn = wrapper.find(Button)
+    btn.props().onClick()
+    btn.props().onClick()
+
+    wrapper.update()
+
+    expect(wrapper.find("div.Notification")).toHaveLength(2)
+
+    setTimeout(() => {
+      wrapper.update()
+
+      expect(wrapper.find("div.Notification")).toHaveLength(1)
+    }, displayInterval)
+
+    setTimeout(() => {
+      wrapper.update()
+
+      expect(wrapper.find("div.Notification")).toHaveLength(0)
+    }, displayInterval)
   })
 })
